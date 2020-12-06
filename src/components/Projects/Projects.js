8d303f8800af738e0acfca81fe307020ca878d3f
josilob <bjosilo@gmail.com>
2020-12-06 15:55:42 -0500

    project containers

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,64 @@ function Projects() {
 		<div id='projects'>
 			<h2>Below, I will share some of my previously made projects</h2>
 			<div className='projects-div'>
-				<div className='project-card'>
+				<div className='card'>
+					<img src='cineprime.png' alt='project' />
+					<p className='title'>Cineprime</p>
+					<div className='overlay'></div>
+					<div className='button-wrap'>
+						<a
+							href='https://github.com/josilob/cineprime-frontend'
+							target='_blank'>
+							<button className='btn'>GitHub</button>
+						</a>
+						<a href='https://cineprime.netlify.app/' target='_blank'>
+							<button className='btn'>Live</button>
+						</a>
+					</div>
+				</div>
+				<div className='card'>
+					<img src='MDL.png' alt='project' />
+					<p className='title'>Mixed Drinks Library</p>
+					<div className='overlay'></div>
+					<div className='button-wrap'>
+						<a
+							href='https://github.com/josilob/Mixed-Drinks-Library'
+							target='_blank'>
+							<button className='btn'>GitHub</button>
+						</a>
+						<a href='https://mixed-drinks-josilob.netlify.app/' target='_blank'>
+							<button className='btn'>Live</button>
+						</a>
+					</div>
+				</div>
+				<div className='card'>
+					<img src='Hops.png' alt='project' />
+					<p className='title'>Hops Revolution</p>
+					<div className='overlay'></div>
+					<div className='button-wrap'>
+						<a href='https://github.com/josilob/Project4' target='_blank'>
+							<button className='btn'>GitHub</button>
+						</a>
+						<a href='https://brewery-cap.netlify.app/' target='_blank'>
+							<button className='btn'>Live</button>
+						</a>
+					</div>
+				</div>
+
+				<div className='card'>
+					<img src='tunr.png' alt='project' />
+					<p className='title'>TUNR</p>
+					<div className='overlay'></div>
+					<div className='button-wrap'>
+						<a href='https://github.com/josilob/TUNR-front' target='_blank'>
+							<button className='btn'>GitHub</button>
+						</a>
+						<a href='https://boj-tunr.netlify.app/' target='_blank'>
+							<button className='btn'>Live</button>
+						</a>
+					</div>
+				</div>
+				{/* <div className='project-card'>
 					<div className='btn-wrap'>
 						<a
 							href='https://github.com/josilob/cineprime-frontend'
@@ -18,6 +75,7 @@ function Projects() {
 						</a>
 					</div>
 				</div>
+
 				<div className='project-card'>
 					<div className='btn-wrap'>
 						<a
@@ -30,7 +88,8 @@ function Projects() {
 						</a>
 					</div>
 				</div>
-				<div className='project-card'>
+
+				<div className='container'>
 					<div className='btn-wrap'>
 						<a href='https://github.com/josilob/Project4' target='_blank'>
 							<button className='btn'>GitHub</button>
@@ -49,7 +108,7 @@ function Projects() {
 							<button className='btn'>Live</button>
 						</a>
 					</div>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
